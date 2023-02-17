9794899fc4c22ced7a74fc5102ef04c72040eba3
Harshit <96296921+Hype16@users.noreply.github.com>
2023-02-17 10:54:30 +0530

    Temp ₹ update

--- script.js
+++ script.js
@@ -78,9 +78,9 @@ function updateValues() {
     -1
   ).toFixed(2);
 
-  balance.innerText = `$${total}`;
-  money_plus.innerText = `$${income}`;
-  money_minus.innerText = `$${expense}`;
+  balance.innerText = `₹${total}`;
+  money_plus.innerText = `₹${income}`;
+  money_minus.innerText = `₹${expense}`;
 }
 
 //6
